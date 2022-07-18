79d16b14a056e64bb69f364595a80c88e0c31bd1
https://github.com/islomrakhimkulov <islomraximqulov448@gmail.com>
2022-07-18 19:49:07 +0500

    u

--- src/pages/tablet/voting-question-final-result.vue
+++ src/pages/tablet/voting-question-final-result.vue
@@ -95,11 +95,11 @@
 							</p>
 
 							<!-- chart result  -->
-							<div class="pb-2">
+							<div class="py-2">
 								<h2 class="text-[14px] text-gray-900 uppercase">
 									Ovoz berish jarayoni
 								</h2>
-								<div class="text-center overflow-x-hidden">
+								<div class="text-center overflow-x-hidden py-8">
 									<div class="flex items-center">
 										<div>
 											<VotesCountChart
@@ -116,7 +116,7 @@
 							</div>
 						</div>
 
-						<div class="flex justify-between items-center pt-36">
+						<div class="flex justify-between items-center pt-16">
 							<AppButton size="small" color="disabled"
 								>orqaga</AppButton
 							>
