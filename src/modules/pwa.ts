99fffb9db7ca39f11e2f61a7032b414654eeca19
Islom <69111193+islomrakhimkulov@users.noreply.github.com>
2022-09-14 15:58:14 +0500

    style: fixed file names of project (#106)

--- src/modules/pwa.ts
+++ src/modules/pwa.ts
@@ -1,10 +1,10 @@
-import { ViteSetupModule } from '@/types/ViteSetupModule';
+import { ViteSetupModule } from '@/types/vite-setup-module';
 
 export const install: ViteSetupModule = ({ isClient, router }) => {
-	if (!isClient) return;
+  if (!isClient) return;
 
-	router.isReady().then(async () => {
-		const { registerSW } = await import('virtual:pwa-register');
-		registerSW({ immediate: true });
-	});
+  router.isReady().then(async () => {
+    const { registerSW } = await import('virtual:pwa-register');
+    registerSW({ immediate: true });
+  });
 };
